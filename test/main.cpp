9834b9e7074f0aafd1c1c48ec0a9f2567359654e
flagrans <flagrans@mail.ru>
2021-03-26 22:38:47 +0700

    Test project

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Привет мир!" << std::endl;
+    std::string st;
+    std::cin >> st;
+    std::cout << st << std::endl;
+
+    return 0;
+}
